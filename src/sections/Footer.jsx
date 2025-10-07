7e6459c48d1400af120131db8c03140af3bfb43a
Stephenasembo <stephenasembo524@gmail.noreply.github.com>
2025-10-07 17:40:28 +0300

    Feat: Make Navigation Bar Responsive

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,8 +1,18 @@
+import reactIcon from "../assets/icons/react.svg"
+
 export default function Footer() {
   return (
     <div className="mx-2">
       <p className="text-center mb-2"><span>&copy;</span> 2025 Stephen Asembo</p>
-      <p className="text-center">Made with React</p>
+      <p className="text-center">
+        Made with React
+        <img
+        className="inline-block ml-2"
+        src={reactIcon}
+        width={32}
+        height={32}
+        />  
+      </p>
     </div>
   )
 }
--- src/sections/NavigationBar.jsx
+++ src/sections/NavigationBar.jsx
@@ -7,10 +7,10 @@ export default function NavigationBar() {
 
   return (
     <div>
-      <div className="m-2 grid grid-cols-2 items-center">
+      <div className="m-2 grid grid-cols-2 items-center lg:grid-cols-[max-content_1fr]">
         <span className="border-1 w-[max-content] p-2 text-xl font-bold rounded-xl">SA</span>
         <button
-        className="border-1 w-[max-content] justify-self-end p-1 rounded-lg"
+        className="border-1 w-[max-content] justify-self-end p-1 rounded-lg lg:hidden"
         onClick={() => setNavExpanded(!navExpanded)}
         >
           <img
@@ -19,30 +19,36 @@ export default function NavigationBar() {
           height={32}
           />
         </button>
+        <div className="hidden lg:block justify-self-end mr-4">
+          <Links />
+        </div>
       </div>
       <div>
         {navExpanded && <Links />}
       </div>
-      </div>
+    </div>
   )
 }
 
 function Links() {
   return (
-    <div className="m-2 text-lg">
-      <div className="flex items-center">
+    <div className="m-2 text-lg lg:flex lg:gap-[1rem]">
+      <div className="flex items-center lg:hidden">
         <label htmlFor="dark">Dark mode</label>
         <input
         className="w-[3ch] h-[2ch] ml-1"
         type="checkbox" name="dark" id="dark" />
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col lg:flex-row lg:gap-[1rem]">
         <a href="#about">About</a>
         <a href="#projects">Projects</a>
         <a href="#services">Services</a>
         <a href="#contact">Contact</a>
       </div>
       <button>Hire Me</button>
+      <div className="hidden lg:block">
+        <span>Dark mode toggle</span>
+      </div>
     </div>
   )
 }
