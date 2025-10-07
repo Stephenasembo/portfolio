691370a9c7be5f8568788c6c570c8356be7e9ec6
Stephenasembo <stephenasembo524@gmail.noreply.github.com>
2025-10-07 10:45:24 +0300

    Feat: Create About Structure

---
+++ src/sections/About.jsx
@@ -0,0 +1,52 @@
+import icon from "../../public/vite.svg"
+
+export default function About() {
+  return (
+    <div className="mx-2">
+      <h1 className="text-4xl my-2 font-extrabold">Stephen Asembo</h1>
+      <h2 className="text-2xl my-4 font-medium">Full stack web developer</h2>
+      <p className="my-8">
+        I build intuitive web apps focusing on optimized performance and clean UI.
+      </p>
+      <ul className="flex gap-[1rem] my-8">
+        <li>
+          <img
+          src={icon}
+          width={32}
+          height={32}
+          />
+        </li>
+        <li>
+          <img
+          src={icon}
+          width={32}
+          height={32}
+          />
+        </li>
+        <li>
+          <img
+          src={icon}
+          width={32}
+          height={32}
+          />
+        </li>
+        <li>
+          <img
+          src={icon}
+          width={32}
+          height={32}
+          />
+        </li>
+      </ul>
+      <div className="">
+        <h2 className="text-xl mt-16 mb-8">ABOUT</h2>
+        <p className="my-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur velit suscipit perferendis provident excepturi, dolores quod tenetur molestias accusamus ratione quos sit et doloremque ullam voluptatibus enim quibusdam? Possimus, recusandae?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab inventore minima quibusdam temporibus quo. Eaque saepe deserunt in ad eius veniam, similique qui necessitatibus, nam minus sit quidem labore ducimus.
+        </p>
+      </div>
+    </div>
+  )
+}
