3ed913e102e13c81d0eec89f169a2048d7d48a61
Stephenasembo <165886904+Stephenasembo@users.noreply.github.com>
2025-10-12 22:44:17 +0300

    Feat: Setup Contact Form Endpoint
    Messages from visitors can now be received

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -12,7 +12,10 @@ export default function Contact() {
       <p>
         Enter your contact details below and I will get back to you.
       </p>
-      <form className="flex flex-col gap-2 mt-2">
+      <form
+      action="https://getform.io/f/ayveogyb"
+      method="POST"
+      className="flex flex-col gap-2 mt-2">
         <p>
           Fields marked with an asterisk(*) are required.
         </p>
@@ -20,23 +23,26 @@ export default function Contact() {
           <label htmlFor="name">Name:*</label>
           <input
           className="border-1 block rounded-lg w-full py-2 px-[12px] mt-1 h-[48px] max-w-[480px]"
-          type="text" id="name" name="name" placeholder="Your name"/>
+          type="text" id="name" name="name" placeholder="Your name" required/>
         </div>
         <div>
           <label htmlFor="email">Email:*</label>
           <input
           className="border-1 block rounded-lg w-full py-2 px-[12px] mt-1 h-[48px] max-w-[480px]"
-          type="email" id="email" name="email" placeholder="johndoe@gmail.com"
+          type="email" id="email" name="email" placeholder="johndoe@gmail.com" required
           />
         </div>
         <div>
           <label htmlFor="message">Message:*</label>
           <textarea
           className="border-1 block rounded-lg w-full py-2 px-[12px] mt-1 max-w-[480px]"
-          id="message" name="message" placeholder="Your message..."
+          id="message" name="message" placeholder="Your message..." required
           ></textarea>
         </div>
+        {/* Prevent spam */}
+        <input type="hidden" name="_gotcha" style={{display: "none"}}></input>
         <button
+        type="submit"
         className="w-[max-content] block mx-auto md:mx-0 p-1 rounded-lg px-2 my-2 bg-sky-500 cursor-pointer text-slate-50 hover:scale-125 hover:bg-sky-600 duration-300 ease-out font-semibold ring-2 ring-sky-400/30"
         >Send message</button>
       </form>
